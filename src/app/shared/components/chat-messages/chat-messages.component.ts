61822c23b1aadd88708158038c709dc33d42acd2
Emito Khoza <emito.khoza@gmail.com>
2024-08-13 23:48:07 +0200

    Added loading

--- src/app/shared/components/chat-messages/chat-messages.component.ts
+++ src/app/shared/components/chat-messages/chat-messages.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { ChatMessageComponent } from '../chat-message/chat-message.component';
 import { ChatMessageInterface } from '../../models/chat-message.interface';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 
 @Component({
   selector: 'app-chat-messages',
   standalone: true,
   imports: [
-    ChatMessageComponent
+    ChatMessageComponent,
+    MatProgressSpinnerModule
   ],
   templateUrl: './chat-messages.component.html',
   styleUrl: './chat-messages.component.css'
 })
 export class ChatMessagesComponent {
   @Input() chats: ChatMessageInterface[] = [];
+  @Input() loading: boolean = false;
 }
