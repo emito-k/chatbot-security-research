b5210660ccaa229f8dc6d76636486f53e5c27183
Emito Khoza <emito.khoza@gmail.com>
2024-08-13 23:21:41 +0200

    Added chat messages component

---
+++ src/app/shared/components/chat-messages/chat-messages.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { ChatMessageComponent } from '../chat-message/chat-message.component';
+import { ChatMessageInterface } from '../../models/chat-message.interface';
+
+@Component({
+  selector: 'app-chat-messages',
+  standalone: true,
+  imports: [
+    ChatMessageComponent
+  ],
+  templateUrl: './chat-messages.component.html',
+  styleUrl: './chat-messages.component.css'
+})
+export class ChatMessagesComponent {
+   @Input() chats: ChatMessageInterface[] = [];
+}
