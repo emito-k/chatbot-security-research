40516009046ed93b9c89a8f38f3a56330cd5acbc
Emito Khoza <emito.khoza@gmail.com>
2024-10-15 13:54:18 +0200

    Updated the server

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 // import conversationRoutes from "./routes/conversation.route.js";
 // import messageRoutes from "./routes/message.route.js";
-
+import sequelize from "./models/index.js";
 dotenv.config();
 
 const app = express();
@@ -15,6 +15,13 @@ app.use("/users", userRoutes);
 // app.use("/conversations", conversationRoutes);
 // app.use("/messages", messageRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch((error) => {
+  console.log(error);
+  console.error("Failed to connect to database");
 });
+
