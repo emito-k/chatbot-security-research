54718481ea72a9ba923cec3705c131b4bf34ecd1
Emito Khoza <emito.khoza@gmail.com>
2024-10-15 10:29:03 +0200

    Added user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,15 @@
+// models/user.model.js
+import { DataTypes } from "sequelize";
+import sequelize from "./index.js";
+
+const User = sequelize.define("User", {
+  id: { type: DataTypes.BIGINT.UNSIGNED, autoIncrement: true, primaryKey: true },
+  username: { type: DataTypes.STRING(255), allowNull: false },
+  password: { type: DataTypes.STRING(255), allowNull: false },
+  public_key: { type: DataTypes.BIGINT, allowNull: false },
+  is_bot: { type: DataTypes.BOOLEAN, allowNull: false }
+}, {
+  timestamps: false,
+});
+
+export default User;
