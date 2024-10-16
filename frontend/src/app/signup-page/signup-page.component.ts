0a67c087625e163dd90e35c5c9320a2ef3fb7173
Emito Khoza <emito.khoza@gmail.com>
2024-10-16 19:26:27 +0200

    Updated signup page

--- frontend/src/app/signup-page/signup-page.component.ts
+++ frontend/src/app/signup-page/signup-page.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css'
 })
 export class SignupPageComponent {
+  snackBar = inject(MatSnackBar);
 
+  usernameFormControl = new FormControl("");
+  passwordFormControl = new FormControl("")
+
+  onSubmit(event: any) {
+    event.preventDefault()
+
+    if (this.usernameFormControl.value && this.passwordFormControl.value) {
+      this.snackBar.open("Yippie");
+      console.log(this.usernameFormControl.value, this.passwordFormControl.value);
+    }
+    else {
+      this.snackBar.open("Please provide valid input");
+    }
+  }
 }
