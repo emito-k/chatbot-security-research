bab63f3f856cb1f825d946058c0890d6c5185fcc
Emito Khoza <emito.khoza@gmail.com>
2024-10-16 18:51:51 +0200

    Added frontend pages

---
+++ frontend/src/app/conversation-page/conversation-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conversation-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './conversation-page.component.html',
+  styleUrl: './conversation-page.component.css'
+})
+export class ConversationPageComponent {
+
+}
